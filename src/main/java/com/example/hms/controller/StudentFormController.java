09d55002e8115208bd1983f802eda7e1d88271bb
ShanPereraX <perera.alc2000@gmail.com>
2023-04-01 01:31:04 +0530

    Student Ui : Done

--- src/main/java/com/example/hms/controller/StudentFormController.java
+++ src/main/java/com/example/hms/controller/StudentFormController.java
@@ -1,4 +1,98 @@
 package com.example.hms.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TableView<?> tblStudents;
+
+    @FXML
+    private TableColumn<?, ?> colStudentId;
+
+    @FXML
+    private TableColumn<?, ?> colStudentName;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colGender;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private RadioButton rBtnMale;
+
+    @FXML
+    private ToggleGroup gender;
+
+    @FXML
+    private RadioButton rBtnFemale;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnKeyReleased(KeyEvent event) {
+
+    }
+
 }
