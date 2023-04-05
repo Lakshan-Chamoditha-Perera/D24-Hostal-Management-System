087277af092d9aab64fc17d1ba1215b2c5555f7e
ShanPereraX <perera.alc2000@gmail.com>
2023-04-05 23:22:47 +0530

    add unpaid students form

--- src/main/java/com/example/hms/controller/UnpaidCaseFormController.java
+++ src/main/java/com/example/hms/controller/UnpaidCaseFormController.java
@@ -1,4 +1,43 @@
 package com.example.hms.controller;
 
-public class UnpaidCaseFormController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UnpaidCaseFormController implements Initializable {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TableView<?> tblstudents;
+
+    @FXML
+    private TableColumn<?, ?> colStdId;
+
+    @FXML
+    private TableColumn<?, ?> colStdName;
+
+    @FXML
+    private TableColumn<?, ?> colStdAddress;
+
+    @FXML
+    private TableColumn<?, ?> colMobile;
+
+    @FXML
+    void tblstudentsOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
 }
