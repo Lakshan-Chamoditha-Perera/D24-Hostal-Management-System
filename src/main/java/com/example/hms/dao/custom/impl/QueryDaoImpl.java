765f88fb06264bd4b50320ffd2abc73da4c892ce
ShanPereraX <perera.alc2000@gmail.com>
2023-04-05 23:59:14 +0530

    join query added

--- src/main/java/com/example/hms/dao/custom/impl/QueryDaoImpl.java
+++ src/main/java/com/example/hms/dao/custom/impl/QueryDaoImpl.java
@@ -2,12 +2,15 @@ package com.example.hms.dao.custom.impl;
 
 import com.example.hms.dao.custom.QueryDao;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class QueryDaoImpl implements QueryDao {
     @Override
     public List getUnpaidStudents(Session session) {
-        return null;
+        Query query = session.createQuery("SELECT DISTINCT s FROM Student s LEFT JOIN s.reservationList r LEFT JOIN r.room rm WHERE r.status = 'un-paid'");
+        List list = query.list();
+        return list;
     }
 }
